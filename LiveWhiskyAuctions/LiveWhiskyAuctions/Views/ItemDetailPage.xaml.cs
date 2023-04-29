7a8f501484c27cdff06af7ce3d2e3835b0fe50ca
Peter Clark <peter.clark@arnlea.com>
2023-04-29 14:28:36 +0100

    Add project files.

---
+++ LiveWhiskyAuctions/LiveWhiskyAuctions/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LiveWhiskyAuctions.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace LiveWhiskyAuctions.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ LiveWhiskyAuctions/LiveWhiskyAuctions/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using LiveWhiskyAuctions.Models;
+using LiveWhiskyAuctions.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LiveWhiskyAuctions.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
