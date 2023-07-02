e1f9726bebbc22b16c1dfec41b061ec526640bcd
Peter Clark <peter.clark@arnlea.com>
2023-07-02 16:02:22 +0100

    Updated styling

--- LiveWhiskyAuctions/LiveWhiskyAuctions/Views/AboutPage.xaml.cs
+++ LiveWhiskyAuctions/LiveWhiskyAuctions/Views/AboutPage.xaml.cs
@@ -22,16 +22,26 @@ namespace LiveWhiskyAuctions.Views
 
         private const string AuctionsUrl = "https://whiskyhunter.net/events/";
 
+        public List<Auction> AuctionsList { get; private set; }    
+
         public AboutPage()
         {
             InitializeComponent();
             LoadAuctionsAsync();
+
+            // Start the countdown timer
+            //Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            //{
+            //    UpdateTimeRemaining();
+            //    return true;
+            //});
         }
 
         private async void LoadAuctionsAsync()
         {
             var auctions = await ScrapeAuctionsAsync();
             auctionsListView.ItemsSource = auctions;
+            AuctionsList = auctions;
         }
 
         private async Task<List<Auction>> ScrapeAuctionsAsync()
@@ -54,18 +64,17 @@ namespace LiveWhiskyAuctions.Views
 
                 foreach (var node in auctionNodes)
                 {
-                    var auctionColumns = node.Descendants("td").ToList();
+                    var auctionColumns = node.Descendants("td").ToList();                   
 
-                    if(auctionColumns.Count > 0)
-                    {
+                    if (auctionColumns.Count > 0)
+                    {               
                         var auction = new Auction
                         {
-                            Name = auctionColumns[0].Descendants("a").FirstOrDefault()?.InnerText.Trim(),
-                            StartTime = auctionColumns[1]?.InnerText.Trim(),
+                            Name = auctionColumns[0].Descendants("a").FirstOrDefault()?.InnerText.Trim(),                           
                             EndTime = auctionColumns[2]?.InnerText.Trim()                            
-                        };
+                        };                       
 
-                         auction.GetLogoAsync();
+                        auction.GetLogoAsync();
 
                         auctions.Add(auction);
                     }
@@ -76,8 +85,8 @@ namespace LiveWhiskyAuctions.Views
                 {
                     Name = "The Whisky Shop Auction",
                     StartTime = "Varies Per Lot",
-                    EndTime = "Varies Per Lot"
-
+                    EndTime = "Varies Per Lot",
+                    Link = "https://www.whiskyshop.com/auctions/lots"
                 };
 
                 auctions.Add(whiskyshopAuction);
@@ -86,7 +95,26 @@ namespace LiveWhiskyAuctions.Views
             }
             return null;
         }
-       
+
+        private void UpdateTimeRemaining()
+        {
+            foreach (var auction in AuctionsList)
+            {
+                if(auction.Name != "The Whisky Shop Auction")
+                {
+                    var timeRemaining = DateTime.Parse(auction.EndTime) - DateTime.Now;
+
+                    if (timeRemaining.TotalSeconds <= 0)
+                    {
+                        auction.TimeRemaining = "Auction Ended";
+                    }
+                    else
+                    {
+                        auction.TimeRemaining = $"{timeRemaining.Days}d {timeRemaining.Hours}h {timeRemaining.Minutes}m {timeRemaining.Seconds}s";
+                    }
+                }                
+            }
+        }
         private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             if (e.SelectedItem == null)
@@ -106,6 +134,7 @@ namespace LiveWhiskyAuctions.Views
         public string EndTime { get; set; }
         public string Logo { get; set; }
         public string Link { get; set; }
+        public string TimeRemaining { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -113,7 +142,7 @@ namespace LiveWhiskyAuctions.Views
         {
             if (!string.IsNullOrEmpty(Logo))
                 return;
-
+            Name = Name.Replace("-", "").Replace(" ", "");
             if (Name.ToLower().Contains("unicorn"))
             {
                 Link = "https://bid.unicornauctions.com/";
@@ -139,7 +168,7 @@ namespace LiveWhiskyAuctions.Views
                 Link = "https://whisky.auction/auctions/live";
                 return;
             }
-            if (Name.ToLower().Contains("whisky-online"))
+            if (Name.ToLower().Contains("whiskyonline"))
             {
                 Link = "https://live.whisky-onlineauctions.com/";
                 return;
@@ -154,11 +183,11 @@ namespace LiveWhiskyAuctions.Views
                 Link = "https://whiskyauctioneer.com/current-auction";
                 return;
             }
-            if (Name.ToLower() == "whiskyauction")
+            if (Name.ToLower().Contains("whiskyauction"))
             {
                 Link = "https://whiskyauction.com/wac/auctionBrowser";
                 return;
-            }
+            }           
             if (Name.ToLower().Contains("thewhiskyshop"))
             {
                 Link = "https://www.whiskyshop.com/auctions/lots";
